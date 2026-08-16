6f6ac4acc3e8cdce78bd35a026e35c0aac0874ad
thongkeosivilay-afk <thongkeosivilay@gmail.com>
2026-08-16 17:09:18 +0700

    Delete script.js

--- script.js
+++
@@ -1,5 +0,0 @@
-  // scroll reveal
-  const io = new IntersectionObserver((entries) => {
-    entries.forEach(en => { if (en.isIntersecting) en.target.classList.add('in'); });
-  }, { threshold: 0.12 });
-  document.querySelectorAll('.reveal').forEach(el => io.observe(el));
